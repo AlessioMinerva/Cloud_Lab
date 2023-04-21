fc9b5bba6c5aa8c19acaf03df72222b0ec200453
Alessio Minerva <70346207+AlessioMinerva@users.noreply.github.com>
2023-04-21 10:46:17 +0100

    Update variables.tf

--- Modules/VPC/variables.tf
+++ Modules/VPC/variables.tf
@@ -6,4 +6,4 @@ variable "environment" {}
 # Vpc variables 
 variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
-variable "private_app_subnet_az1_cidr" {}
+variable "private_subnet_az1_cidr" {}
