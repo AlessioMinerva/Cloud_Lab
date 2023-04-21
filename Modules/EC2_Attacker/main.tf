ba57644bab8e5c5e25f70d179ae0c5dc2146b08b
Alessio Minerva <70346207+AlessioMinerva@users.noreply.github.com>
2023-04-21 10:33:35 +0100

    Update main.tf

--- Modules/EC2_Attacker/main.tf
+++ Modules/EC2_Attacker/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_instance" {
   ami                    = var.ami_attacker
   instance_type          = "t2.micro"
   subnet_id              = var.public_subnet_az1_id
-  vpc_security_group_ids = [var.attacker_security_group_id]
+  vpc_security_group_id = var.attacker_security_group_id
   key_name               = var.key
 
    tags = {
