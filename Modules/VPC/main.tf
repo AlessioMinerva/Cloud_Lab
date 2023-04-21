1d0243fc3d062d5c5d83da5007ff44247d0b3549
Alessio Minerva <70346207+AlessioMinerva@users.noreply.github.com>
2023-04-21 10:44:57 +0100

    Update main.tf

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -54,14 +54,14 @@ resource "aws_route_table_association" "public_subnet_az1_rt_association" {
 }
 
 
-# create private app subnet az1
-resource "aws_subnet" "private_app_subnet_az1" {
+# create private subnet az1
+resource "aws_subnet" "private_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private_app_subnet_az1_cidr
+  cidr_block              = var.private_subnet_az1_cidr
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-private-app-az1"
+    Name = "${var.project_name}-${var.environment}-private-az1"
   }
 }
